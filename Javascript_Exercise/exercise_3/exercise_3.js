a38752f6d12f91339826503cb537cc59d8fb4b76
morkraksa <141297181+MORK-RAKSA@users.noreply.github.com>
2024-04-08 16:10:41 +0700

    Update

--- Javascript_Exercise/exercise_3/exercise_3.js
+++ Javascript_Exercise/exercise_3/exercise_3.js
@@ -1,84 +1,77 @@
 // 1. Write a function called that takes an array of numbers and a callback
 // function that processes each element of the array. Use the callback to
 // square each number in the array
-function sqArray(arr, callback) {
-if (!Array.isArray(arr)) {
-    return "Please provide an array";
-}
-    let result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(callback(arr[i]))
-    }
-    return result
-}
-let number = [1, 2, 3, 4, 5];
-function square(num) {
-    return num * num
-}
-console.log(sqArray(number, square))
+// let list = [1,2,3,4,5,6,7,8,9]
+// const callSqare = (array, callback) => {
+//   let x = [];
+//   for(let i of array)
+//       x.push(callback(i));
+//   return x;
+// }
+// const sqare = (num) => num * num;
+// console.log(callSqare(list, sqare))
 
 // 2. Create a function that takes an array of numbers and a callback.
 // The callback should return true if a number is even. Use the callback to filter the array synchronously.
-function arrayEventNum(arr, eventNUm) {
-    const evenNumbers = [];
-    for (const num of arr) {
-      if (eventNUm(num)) {
-        evenNumbers.push(num);
-      }
+const eventNum = (num) => num % 2 === 0;
+function arrayEventNum(arr, eventNum) {
+  const evenNumbers = [];
+  for (const num of arr) {
+    if (eventNum(num)) {
+      evenNumbers.push(num);
     }
-    return evenNumbers;
   }
-  const numbers = [1, 2, 3, 4, 5, 6];
-  const eventNUm = (num) => num % 2 === 0;
-  const evenFilteredNumbers = arrayEventNum(numbers, eventNUm);
-  console.log(evenFilteredNumbers);
+  return evenNumbers;
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arrayEventNum(numbers, eventNum));
 
 // 3. Use Node.js's fs module to read a file asynchronously:`fs.readFile`.
 // Write a function that takes a file path and a callback. Use the callback
 // to print the contents of the file to the console.
-const fs = require('fs');
-function readFileAsync(filepath, callback) {
-  fs.readFile(filepath, 'utf8', (e, data) => {
-    if (e) {
-      console.error(e);
-      callback(e);
-    } else {
-      callback(null, data);
-    }
-  });
-}
+// const fs = require('fs');
+// function readFileAsync(filepath, callback) {
+//   fs.readFile(filepath, 'utf8', (e, data) => {
+//     if (e) {
+//       console.error(e);
+//       callback(e);
+//     } else {
+//       callback(null, data);
+//     }
+//   });
+// }
 
-const filepath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/Test_Exercise_3.3.txt';
-readFileAsync(filepath, (e, data) => {
-  if (e) {
-    console.error('Error reading file:', e);
-  } else {
-    console.log(`content: \n${data}`);
-  }
-});
+// const filepath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/Test_Exercise_3.3.txt';
+// readFileAsync(filepath, (e, data) => {
+//   if (e) {
+//     console.error('Error reading file:', e);
+//   } else {
+//     console.log(`content: \n${data}`);
+//   }
+// });
 
 // 4. Use Node.js's fs module to write a file asynchronously `fs.writeFile`. Write a function that takes
 // a file path and a callback. Use the callback to write the contents of the file to the file path.
-const fs = require('fs');
-function writeFile(filePath, content, callback) {
-    fs.writeFile(filePath, content, (err) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null);
-        }
-    });
-}
-let filePath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/Test_Exercise_3.4.txt';
-let fileContent = '[1,2,3,4,5,6,7,8,9,10]';
+// const fs = require('fs');
+// function writeFile(filePath, content, callback) {
+//     fs.writeFile(filePath, content, (err) => {
+//         if (err) {
+//             callback(err);
+//         } else {
+//             callback(null);
+//         }
+//     });
+// }
+// let filePath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/Test_Exercise_3.4.txt';
+// let fileContent = '[1,2,3,4,5,6,7,8,9,10]';
 
-writeFile(filePath, fileContent, (err) => {
-    if (err) {
-        console.error('Error writing to file:', err);
-    } else {
-        console.log('File has been written success.');
-    }
-});
+// writeFile(filePath, fileContent, (err) => {
+//     if (err) {
+//         console.error('Error writing to file:', err);
+//     } else {
+//         console.log('File has been written success.');
+//     }
+// });
 
 //5.Use the example 3 & 4, write in the below scenario:
 //     - Read the context of `input.txt`
@@ -86,9 +79,9 @@ writeFile(filePath, fileContent, (err) => {
 //     - Read `output1.txt` and append “SEcond modification” to the file `output2.txt`
 //     - Finally read `output2.txt` and print to the console
 
-const fs = require('fs');
+const fs = require("fs");
 function readFileAsync(filepath, callback) {
-  fs.readFile(filepath, 'utf8', (err, data) => {
+  fs.readFile(filepath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return callback(err);
@@ -98,7 +91,7 @@ function readFileAsync(filepath, callback) {
 }
 
 function writeFileAsync(filepath, content, callback) {
-  fs.writeFile(filepath, content, 'utf8', (err) => {
+  fs.writeFile(filepath, content, "utf8", (err) => {
     if (err) {
       console.error(err);
       return callback(err);
@@ -107,7 +100,12 @@ function writeFileAsync(filepath, content, callback) {
   });
 }
 
-function modifyAndWriteFile(inputFilepath, outputFilepath, modification, callback) {
+function modifyAndWriteFile(
+  inputFilepath,
+  outputFilepath,
+  modification,
+  callback
+) {
   readFileAsync(inputFilepath, (err, data) => {
     if (err) return callback(err);
 
@@ -119,26 +117,39 @@ function modifyAndWriteFile(inputFilepath, outputFilepath, modification, callbac
   });
 }
 
-const inputFilepath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/input.txt';
-const output1Filepath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/output1.txt';
-const output2Filepath = 'Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/output2.txt';
+const inputFilepath =
+  "Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/input.txt";
+const output1Filepath =
+  "Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/output1.txt";
+const output2Filepath =
+  "Y:/E5-Year3-G25/Web Design/HTML/Chrome Download/Javascript_Exercise/exercise_3/output2.txt";
 
-modifyAndWriteFile(inputFilepath, output1Filepath, 'First modification', (err) => {
-  if (err) {
-    console.error('Error during first modification:', err);
-  } else {
-    modifyAndWriteFile(output1Filepath, output2Filepath, 'Second modification', (err) => {
-      if (err) {
-        console.error('Error during second modification:', err);
-      } else {
-        readFileAsync(output2Filepath, (err, data) => {
+modifyAndWriteFile(
+  inputFilepath,
+  output1Filepath,
+  "First modification",
+  (err) => {
+    if (err) {
+      console.error("Error during first modification:", err);
+    } else {
+      modifyAndWriteFile(
+        output1Filepath,
+        output2Filepath,
+        "Second modification",
+        (err) => {
           if (err) {
-            console.error('Error reading final output:', err);
+            console.error("Error during second modification:", err);
           } else {
-            console.log('Final content:', data);
+            readFileAsync(output2Filepath, (err, data) => {
+              if (err) {
+                console.error("Error reading final output:", err);
+              } else {
+                console.log("Final content:", data);
+              }
+            });
           }
-        });
-      }
-    });
+        }
+      );
+    }
   }
-});
+);
